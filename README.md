b48044b320d85359282203da0286224da2136d9f
Cathy Slider <cslider1@aol.com>
2021-03-05 13:11:34 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# real_estate_purchase
+## Real Estate Purchase Front-End Pages
+
+
+The files in this repository are being developed for use with a Solidity real estate purchase smart contract. 
+
+The various input fields and other designated information will need to transfer to the contract whenever Submit is executed on the pages.
---
+++ js/real_estate.js
@@ -0,0 +1,58 @@
+// Using array-of-objects js as guide
+
+document.getElementById('datePicker').value = new Date().toDateInputValue();
+
+document.querySelector('body').innerHTML = html;
+
+// // 1. Create an array to hold quiz questions and answers
+// const questions = [
+//   {question: 'How many planets are in the Solar System?', answer:'8'},
+//   {question: 'How many continents are there?', answer:'7'},
+//    {question: 'How many legs does an insect have?', answer:'6'},
+//    {question: 'What year was JavaScript created?', answer:'1995'}
+// ];
+
+// // 2. Store the number of questions answered correctly
+// const correct = [];
+// const incorrect = [];
+// let correctAnswers = 0;
+
+// /* 
+//   3. Use a loop to cycle through each question
+//       - Present each question to the user
+//       - Compare the user's response to answer in the array
+//       - If the response matches the answer, the number of correctly
+//         answered questions increments by 1
+// */
+// for ( let i = 0; i < questions.length; i++ ) {
+//   let question = questions[i].question;
+//   let answer = questions[i].answer;
+//   let response = prompt(question);
+  
+//   if ( response === answer ) {
+//     correctAnswers++;
+//     correct.push(question);
+//   } else {
+//     incorrect.push(question);
+//   }
+// }
+
+// function createListItems(arr) {
+//   let items = '';
+//   for (let i = 0; i < arr.length; i++) {
+//     items += `<li>${arr[i]}</li>`;
+//   }
+//   return items;
+// }
+
+// // 4. Display the number of correct answers to the user
+// let html = `
+//   <h1>You got ${correctAnswers} question(s) correct</h1>
+//   <h2>You got these questions right:</h2>
+//   <ol>${ createListItems(correct) }</ol>
+
+//   <h2>You got these questions wrong:</h2>
+//   <ol>${ createListItems(incorrect) }</ol>
+// `;
+
+// document.querySelector('main').innerHTML = html;
