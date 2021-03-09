7b172e56afb0df5862a80639bb4462bc6e21361c
Cathy Slider <cslider1@aol.com>
2021-03-08 21:08:52 -0500

    additions and modifications

---
+++ js/re.js
@@ -0,0 +1,30 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    var d = new Date();
+    document.getElementById("date").innerHTML = d;
+
+console.log(date_field, "date help")
+
+});  // native browser event and callback
+
+// document.addEventListener("DOMContentLoaded", function(){
+//     let date_field = document.getElementById("date");
+//     date_field.value = new Date();
+
+//     // let now = document.getElementById("date"); 
+//     // now.dateFormat("longTime");
+    
+//     // var d = new Date();
+//     // document.getElementById("datePicker").innerHTML = d.toDateString();
+   
+// console.log(date_field, "date help")
+
+
+// });  // native browser event and callback
+
+function initiateTransfer {
+    
+}
+
+
